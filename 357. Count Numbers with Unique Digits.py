3a7adb0f194eee19da9289839e6c65f54c23eaf3
wym1993 <wang532@illinois.edu>
2017-07-13 21:31:54 -0500

    7.13

---
+++ 357. Count Numbers with Unique Digits.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def countNumbersWithUniqueDigits(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        store = [9, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+        ans, product = 1, 1
+        
+        for i in range(n if n<=10 else 10):
+            product*=store[i];
+            ans+=product;
+        
+        return ans;
